37a2dd5757588fc05d11ec548e43676500d407a2
Konstantin Sysuev <harry-run@yandex.ru>
2020-03-25 15:19:47 +0300

    swvwsfv

--- src/http/config.ts
+++ src/http/config.ts
@@ -15,10 +15,3 @@ export const configs = {
 
 // @ts-ignore
 export const config = configs[process.env.NODE_ENV];
-
-export function logVersion() {
-  const styles = [
-    "font-size: 15px; font-weight: bold",
-    "font-size: 15px; color: #007aff"
-  ];
-}
