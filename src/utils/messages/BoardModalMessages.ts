4b7f82a0a895c6250fc01aa7b4a244ae3c2784fe
Konstantin Sysuev <harry-run@yandex.ru>
2020-03-31 14:28:34 +0300

    патапт

---
+++ src/utils/messages/BoardModalMessages.ts
@@ -0,0 +1,15 @@
+interface IModalMessage {
+  title: string;
+  text: string;
+  buttonTitle?: string[];
+}
+
+interface IText {
+  title: string;
+  text: string;
+  buttonTitle?: string[];
+}
+
+export const messages = {
+  rollDices: {}
+};
