9d11bcacb45f852726e50cf3e226b6dfcd4f2aee
Konstantin Sysuev <harry-run@yandex.ru>
2020-04-10 08:30:16 +0300

    wsvdwv

--- src/components/core/PlayersCore/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { RouteComponentProps } from "@reach/router";
-
-interface Props extends RouteComponentProps {}
-
-//TODO unused now, is it needed?
-export const Game = (props: Props) => {
-  return <>GAME</>;
-};
