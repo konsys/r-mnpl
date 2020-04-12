9e7ae3c75e25e0804e9fbbd60075ddc7de3987fc
Konstantin Sysuev <harry-run@yandex.ru>
2020-04-12 11:46:29 +0300

    zdv dzv

--- src/handlers/Modals.ts
+++ src/handlers/Modals.ts
@@ -1,23 +1,25 @@
 import { BoardActionType, BoardAction } from "../types/BoardTypes";
 import { dicesModalEffect } from "../stores/ModalStore";
 
-export const rollDicesModal = (act: BoardAction): BoardAction => ({
-  type: BoardActionType.SHOW_DICES_MODAL,
-  userId: act.userId,
-  title: act.title,
-  text: act.text,
-  actionButtons: [
-    {
-      title: "Бросить кубики",
-      onClick: () => {
-        dicesModalEffect({
-          actionId: act._id,
-        });
+export const rollDicesModal = (act: BoardAction): BoardAction => {
+  return {
+    type: BoardActionType.SHOW_DICES_MODAL,
+    userId: act.userId,
+    title: act.title,
+    text: act.text,
+    actionButtons: [
+      {
+        title: "Бросить кубики",
+        onClick: () => {
+          dicesModalEffect({
+            actionId: act._id,
+          });
+        },
       },
-    },
-  ],
-  _id: act._id,
-});
+    ],
+    _id: act._id,
+  };
+};
 
 // const canBuyModalContent = (act: BoardAction): BoardAction => ({
 //   type: BoardActionType.SHOW_DICES_MODAL,
