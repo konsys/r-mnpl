7539e7bfd844cba88f523b7f8cdba5ff1e14032b
Konstantin Sysuev <harry-run@yandex.ru>
2020-04-23 17:30:14 +0300

    z dc

--- src/handlers/PlayersHandler.ts
+++ src/handlers/PlayersHandler.ts
@@ -2,5 +2,5 @@ import { playersStore, setPlayersEvent } from "../stores/PlayersStore";
 import { IPlayer } from "../types/BoardTypes";
 
 export const playersHandler = (players: IPlayer[]) => {
-  setPlayersEvent({ players, version: playersStore.getState().version });
+  setPlayersEvent({ players, version: ++playersStore.getState().version });
 };
