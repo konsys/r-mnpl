c500586a3ec2232d873e6d5539540187dbf017c2
Konstantin Sysuev <harry-run@yandex.ru>
2020-06-17 18:01:57 +0300

    ascasc

--- src/handlers/MessageHandler.ts
+++ src/handlers/MessageHandler.ts
@@ -3,14 +3,16 @@ import { setCurrentActionEvent } from "../stores/ActionStore";
 
 import { fieldsHandler } from "./FieldsHandler";
 import { playersHandler } from "./PlayersHandler";
+import nanoid from "nanoid";
 
 export const MessageHandler = (message: BoardMessage) => {
   const event = message.data.event.action;
   fieldsHandler(message.data.boardStatus.fields);
   playersHandler(message.data.boardStatus.players);
 
+  console.log(23424234, event);
   setCurrentActionEvent({
-    actionId: event._id,
+    actionId: (event && event._id) || nanoid(4),
     event: message.data.event,
   });
 };
