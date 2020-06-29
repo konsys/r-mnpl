98522ec3452b088a38179db40d69f0fd28d84fd4
Konstantin Sysuev <harry-run@yandex.ru>
2020-06-29 17:00:57 +0300

    srfnbrfhn

--- src/components/views/Board/Board.tsx
+++ src/components/views/Board/Board.tsx
@@ -33,19 +33,7 @@ export const Board = () => {
           fields.map((field, index) => (
             <Field
               key={(field && field.fieldId) || index}
-              fieldPosition={field.fieldPosition}
-              price={field.price}
-              imgSrc={field.imgSrc}
-              fieldGroup={field.fieldGroup}
-              fieldLine={field.fieldLine}
-              name={field.name}
-              fieldSpecial={field.fieldSpecial}
-              fieldCorner={field.fieldCorner}
-              isJail={field.isJail}
-              status={field.status}
-              type={field.type}
-              currency={field.currency}
-              rent={field.rent}
+              {...field}
               onClick={() => {
                 field.fieldId && setFieldActionEvent(field.fieldId);
                 return false;
@@ -58,24 +46,8 @@ export const Board = () => {
               field.fieldGroup && (
                 <FieldActions
                   key={(field && field.fieldId) || index}
-                  fieldGroup={field.fieldGroup}
-                  name={field.name + "1"}
-                  groupName={field.fieldGroupName || ""}
-                  price={{
-                    branchPrice: field.price?.branchPrice || 0,
-                    buyoutPrice: field.price?.buyoutPrice || 0,
-                    pledgePrice: field.price?.pledgePrice || 0,
-                    startPrice: field.price?.startPrice || 0,
-                  }}
+                  {...field}
                   position={{ top: index * 15, left: index * 2 + 380 }}
-                  rent={{
-                    baseRent: field.rent?.baseRent || 0,
-                    oneStar: field.rent?.oneStar || 0,
-                    twoStar: field.rent?.twoStar || 0,
-                    freeStar: field.rent?.freeStar || 0,
-                    fourStar: field.rent?.fourStar || 0,
-                    bigStar: field.rent?.bigStar || 0,
-                  }}
                   isActive={field.fieldId === fieldActionId}
                 />
               )
--- src/components/views/FieldActions/FieldActions.tsx
+++ src/components/views/FieldActions/FieldActions.tsx
@@ -1,33 +1,32 @@
 import React from "react";
-import {
-  IFieldRent,
-  IFieldPrice,
-  IFieldModalPosition,
-} from "../../../types/BoardTypes";
+import { IFieldModalPosition, IField } from "../../../types/BoardTypes";
 
-interface Prop {
+interface Prop extends IField {
   position: IFieldModalPosition;
-  fieldGroup: number;
-  name: string;
-  groupName: string;
-  rent: IFieldRent;
-  price: IFieldPrice;
   isActive: boolean;
 }
 
-export const FieldActions = (prop: Prop) => {
+export const FieldActions = ({
+  fieldGroup,
+  fieldGroupName,
+  price,
+  rent,
+  position,
+  isActive,
+  name,
+}: Prop) => {
   return (
     <div
       className="TableFieldcard"
       style={{
-        top: `${prop.position.top}px`,
-        left: `${prop.position.left}px`,
-        display: prop.isActive ? "block" : "none",
+        top: `${position.top}px`,
+        left: `${position.left}px`,
+        display: isActive ? "block" : "none",
       }}
     >
-      <div className={`TableFieldcard-top _mnpl_color_${prop.fieldGroup}`}>
-        <div className="TableFieldcard-top-main">{prop.name}</div>
-        <div className="TableFieldcard-top-sub">{prop.groupName}</div>
+      <div className={`TableFieldcard-top _mnpl_color_${fieldGroup}`}>
+        <div className="TableFieldcard-top-main">{name}</div>
+        <div className="TableFieldcard-top-sub">{fieldGroupName}</div>
       </div>
       <div className="_bg">
         <div className="TableFieldcard-buttons">
@@ -42,61 +41,69 @@ export const FieldActions = (prop: Prop) => {
               <div className="_title">
                 <span>Базовая рента</span>
               </div>
-              <div className="_value _type_money">{prop.rent.baseRent}</div>
+              <div className="_value _type_money">{rent && rent.baseRent}</div>
             </div>
             <div>
               <div className="_title">
                 <span className="_star_small"></span>
               </div>
-              <div className="_value _type_money">{prop.rent.oneStar}</div>
+              <div className="_value _type_money">{rent && rent.oneStar}</div>
             </div>
             <div>
               <div className="_title">
                 <span className="_star_small"></span>
                 <span className="_star_small"></span>
               </div>
-              <div className="_value _type_money">{prop.rent.twoStar}</div>
-            </div>
-            <div>
-              <div className="_title">
-                <span className="_star_small"></span>
-                <span className="_star_small"></span>
-                <span className="_star_small"></span>
-              </div>
-              <div className="_value _type_money">{prop.rent.freeStar}</div>
+              <div className="_value _type_money">{rent && rent.twoStar}</div>
             </div>
             <div>
               <div className="_title">
                 <span className="_star_small"></span>
                 <span className="_star_small"></span>
                 <span className="_star_small"></span>
+              </div>
+              <div className="_value _type_money">{rent && rent.freeStar}</div>
+            </div>
+            <div>
+              <div className="_title">
+                <span className="_star_small"></span>
+                <span className="_star_small"></span>
+                <span className="_star_small"></span>
                 <span className="_star_small"></span>
               </div>
-              <div className="_value _type_money">{prop.rent.fourStar}</div>
+              <div className="_value _type_money">{rent && rent.fourStar}</div>
             </div>
             <div>
               <div className="_title">
                 <span className="_star_big"></span>
               </div>
-              <div className="_value _type_money">{prop.rent.bigStar}</div>
+              <div className="_value _type_money">{rent && rent.bigStar}</div>
             </div>
           </div>
           <div className="TableFieldcard-data-rows">
             <div>
               <div className="_title">Стоимость поля</div>
-              <div className="_value _type_money">{prop.price.startPrice}</div>
+              <div className="_value _type_money">
+                {(price && price.startPrice) || 0}
+              </div>
             </div>
             <div>
               <div className="_title">Залог поля</div>
-              <div className="_value _type_money">{prop.price.pledgePrice}</div>
+              <div className="_value _type_money">
+                {price && price.pledgePrice}
+              </div>
             </div>
             <div>
               <div className="_title">Выкуп поля</div>
-              <div className="_value _type_money">{prop.price.buyoutPrice}</div>
+              <div className="_value _type_money">
+                {price && price.buyoutPrice}
+              </div>
             </div>
             <div>
               <div className="_title">Покупка филиала</div>
-              <div className="_value _type_money">{prop.price.branchPrice}</div>
+              <div className="_value _type_money">
+                {price && price.branchPrice}
+              </div>
             </div>
           </div>
         </div>
