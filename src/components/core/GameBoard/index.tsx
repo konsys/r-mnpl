4b32f8d6077f4d77c9df0fb3f5c0d06886e9a5d3
Konstantin Sysuev <harry-run@yandex.ru>
2020-07-11 15:04:13 +0300

    svsvsdv

--- src/components/core/GameBoard/index.tsx
+++ src/components/core/GameBoard/index.tsx
@@ -3,9 +3,7 @@ import React from "react";
 import { useStore } from "effector-react";
 
 import { IFieldModalPosition } from "../../../types/types";
-import { userStore } from "../../../stores/UserStore";
 import { GameBoardView } from "./GameBoardView";
-import { Redirect } from "react-router-dom";
 
 export const fieldsActionPosition = (): IFieldModalPosition => {
   return {
@@ -15,7 +13,5 @@ export const fieldsActionPosition = (): IFieldModalPosition => {
 };
 
 export const GameBoard = () => {
-  const user = useStore(userStore);
-
-  return user ? <GameBoardView /> : <Redirect from="/game" to="/" />;
+  return <GameBoardView />;
 };
--- src/components/core/Login/Login.tsx
+++ src/components/core/Login/Login.tsx
@@ -2,8 +2,6 @@ import React from "react";
 import { loginEffect } from "./model/LoginModel";
 import { LoginForm } from "../../views/LoginForm/LoginForm";
 import { Redirect } from "react-router-dom";
-import { useStore } from "effector-react";
-import { userStore } from "../../../stores/UserStore";
 import { getToken } from "./model/TokenModel";
 
 export interface ILoginForm {
@@ -18,11 +16,11 @@ export interface ILoginResponce {
 export const login = (data: ILoginForm) => loginEffect(data);
 
 export const Login = () => {
-  const user = useStore(userStore);
   const token = getToken();
-  return user || token ? (
-    <Redirect from="/" to="/game" />
+  return token ? (
+    <Redirect to="/game" />
   ) : (
+    // <GameBoard />
     <LoginForm onSubmit={login} />
   );
 };
