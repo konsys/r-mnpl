5465c3fd31320fd0dbbdc04022bb4ef3a8c96903
Konstantins <Sysuev>
2020-07-30 18:51:05 +0300

    wefwef

--- src/components/core/BoardCore/BoardCore.tsx
+++ src/components/core/BoardCore/BoardCore.tsx
@@ -1,21 +1,77 @@
+import { BoardMessage, FieldStatus, IPlayer } from "../../../types/types";
 import React, { useEffect } from "react";
+import {
+  fieldsStore,
+  getInitFieldsEffect,
+  setFieldsEvent,
+} from "../../../stores/FieldsStore";
 
 import { Board } from "../../views/Board/Board";
 import { BoardDomain } from "../../../stores/BoardDomain";
 import { BoardLoading } from "../../views/BoardLoading/BoardLoading";
-import { BoardMessage } from "../../../types/types";
 import { SocketActions } from "../../../types/Socket/SocketTypes";
 import { clearNode } from "effector";
 import { errorHandler } from "../../../handlers/ErrorHandler";
-import { fieldsHandler } from "../../../handlers/FieldsHandler";
-import { getInitFieldsEffect } from "../../../stores/FieldsStore";
+import { isEqual } from "lodash";
 import nanoid from "nanoid";
 import openSocket from "socket.io-client";
-import { playersHandler } from "../../../handlers/PlayersHandler";
 import { setCurrentActionEvent } from "../../../stores/ActionStore";
+import { updateAllPlayers } from "../../../utils/players.utils";
 import { useStore } from "effector-react";
 
 export let boardSocket: SocketIOClient.Socket;
+
+export const fieldsHandler = (messageFieldsStatus: FieldStatus[]) => {
+  allFieldsIterate(messageFieldsStatus);
+};
+
+// Not used
+export const statusFieldsIterate = (messageFieldsStatus: FieldStatus[]) => {
+  const store = fieldsStore.getState();
+  let toUpdateStore = false;
+
+  messageFieldsStatus.forEach((status) => {
+    const storeFieldIndex = store.fields.findIndex(
+      (storeField) => storeField.fieldId === status.fieldId
+    );
+
+    const currentField = store.fields[storeFieldIndex];
+
+    if (currentField && !isEqual(status, currentField.status)) {
+      store.fields[storeFieldIndex] = {
+        ...store.fields[storeFieldIndex],
+        status,
+      };
+      toUpdateStore = true;
+    }
+  });
+
+  toUpdateStore && setFieldsEvent({ ...store, version: ++store.version });
+};
+
+const allFieldsIterate = (messageFieldsStatus: FieldStatus[]) => {
+  const store = fieldsStore.getState();
+
+  store.fields &&
+    store.fields.forEach((storeField, index) => {
+      const messageFieldStatus = messageFieldsStatus.find(
+        (messageField) => messageField.fieldId === storeField.fieldId
+      );
+
+      if (messageFieldStatus) {
+        store.fields[index] = {
+          ...store.fields[index],
+          status: messageFieldStatus,
+        };
+      } else {
+        store.fields[index] = {
+          ...store.fields[index],
+          status: undefined,
+        };
+      }
+    });
+  setFieldsEvent({ ...store, version: ++store.version });
+};
 
 export const MessageHandler = (message: BoardMessage) => {
   const event = message.data.event.action;
@@ -27,6 +83,7 @@ export const MessageHandler = (message: BoardMessage) => {
     event: message.data.event,
   });
 };
+export const playersHandler = (players: IPlayer[]) => updateAllPlayers(players);
 
 export const BoardCore = () => {
   useEffect(() => {
--- src/handlers/PlayersHandler.ts
+++
@@ -1,4 +0,0 @@
-import { IPlayer } from "../types/types";
-import { updateAllPlayers } from "../utils/players.utils";
-
-export const playersHandler = (players: IPlayer[]) => updateAllPlayers(players);
