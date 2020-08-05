f4f654d8ad4805a85ec4448eb11b8b08f5008c77
Konstantins <Sysuev>
2020-08-05 17:58:09 +0300

    wefwef

--- src/components/views/Template/Header.tsx
+++ src/components/views/Template/Header.tsx
@@ -1,3 +1,5 @@
+import "./styles/header.scss";
+
 import { Box, Button, Container } from "@material-ui/core";
 
 import { BLOCK_SIZE } from "../../../theme/sizes";
@@ -5,9 +7,11 @@ import React from "react";
 
 export default function Header() {
   return (
-    <Box m={1} style={{ backgroundColor: "green" }}>
-      <Container maxWidth={BLOCK_SIZE.lg} style={{ backgroundColor: "red" }}>
-        <Button>11</Button>
+    <Box m={1}>
+      <Container maxWidth={BLOCK_SIZE.lg}>
+        <div className="wrapper">
+          <Button>11</Button>
+        </div>
       </Container>
     </Box>
   );
