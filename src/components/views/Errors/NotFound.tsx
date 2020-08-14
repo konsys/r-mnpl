e40f9b9911634aefb4cd70e0dfbff0d8bf0ccbac
Konstantins <Sysuev>
2020-08-14 10:20:35 +0300

    wefwef

--- src/components/views/Errors/NotFound.tsx
+++ src/components/views/Errors/NotFound.tsx
@@ -1,5 +1,6 @@
 import { Button, Grid, Typography } from "@material-ui/core";
 
+import { GRID_SPACING } from "../../../theme";
 import { ReactComponent as NotFoundIcon } from "../../../theme/svg/404.svg";
 import React from "react";
 import Template from "../Template/Template";
@@ -8,7 +9,13 @@ import { useTranslation } from "react-i18next";
 export default function NotFound() {
   const { t } = useTranslation();
   const component = (
-    <Grid container alignItems="center" justify="center" direction="column">
+    <Grid
+      container
+      alignItems="center"
+      justify="center"
+      direction="column"
+      spacing={GRID_SPACING}
+    >
       <Grid item style={{ height: "200px", width: "500px" }}>
         <NotFoundIcon />
       </Grid>
@@ -23,7 +30,11 @@ export default function NotFound() {
         </Typography>
       </Grid>
       <Grid item>
-        <Button>{t("To main page")}</Button>
+        <Button size={"small"} variant="contained" color="primary">
+          <Typography variant="body2" noWrap>
+            {t("To main page")}{" "}
+          </Typography>
+        </Button>
       </Grid>
     </Grid>
   );
