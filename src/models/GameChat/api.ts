a2fb823bafc861e5ae610f497ac54b9210a6e858
Konstantins <Sysuev>
2020-08-18 13:58:12 +0300

    r4f4rf

--- src/models/GameChat/api.ts
+++ src/models/GameChat/api.ts
@@ -1,6 +1,7 @@
+import { IChatMessage } from "../../components/core/Game/GameChat/ChatMessage";
 import { client } from "../../http/client";
 
 const url = `/chat`;
-export const fetchChat = async (ids?: number[]): Promise<any> => {
+export const fetchChat = async (ids?: number[]): Promise<IChatMessage[]> => {
   return await (await client.get(url, { params: { ids } })).data;
 };
--- src/stores/Game/GameChatStore.ts
+++ src/stores/Game/GameChatStore.ts
@@ -1,27 +1,18 @@
 import { GameDomain } from "./UserStore";
+import { IChatMessage } from "../../components/core/Game/GameChat/ChatMessage";
 import { fetchChat } from "../../models/GameChat/api";
-
-export const r = "wsfwef";
 
 const ChatDomain = GameDomain.domain("ChatDomain");
 
-
 export const resetChatEvent = ChatDomain.event();
 
-export const getChatEffect = ChatDomain.effect<any, any>({
+export const getChatEffect = ChatDomain.effect<any, IChatMessage[]>({
   handler: fetchChat,
 });
 
-export const setChatEvent = ChatDomain.event<any>();
+export const setChatEvent = ChatDomain.event<IChatMessage>();
 
-export const chatStore = ChatDomain.store<any>({
-  isActive: false,
-  isBlocked: false,
-  name: "",
-  userId: -1,
-  vip: false,
-  avatar: "",
-  createdAt: new Date(),
-  registrationType: "",
-  team: undefined,
-  
+export const chatStore = ChatDomain.store<IChatMessage[]>([]).on(
+  getChatEffect,
+  (_, v) => v
+);
