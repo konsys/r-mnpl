7beb33422e2ebc02cbe24bceab8849dd843f623c
Konstantins <Sysuev>
2020-08-25 13:33:38 +0300

    sdcsdcsdc

--- src/components/core/Game/FindGames/GameParams/RegularGameParams.tsx
+++ src/components/core/Game/FindGames/GameParams/RegularGameParams.tsx
@@ -1,20 +1,88 @@
-import { Grid, Typography } from "@material-ui/core";
+import {
+  Button,
+  ButtonGroup,
+  Grid,
+  Switch,
+  Typography,
+} from "@material-ui/core";
+import React, { useState } from "react";
 
 import { GRID_SPACING } from "../../../../../theme";
-import React from "react";
 import { useTranslation } from "react-i18next";
+
+interface IState {
+  playersNumber: number;
+  autostart: boolean;
+  privateGame: boolean;
+}
 
 export default function RegularGameParams() {
   const { t } = useTranslation();
+  const [state, setState] = useState<IState>({
+    autostart: false,
+    playersNumber: 4,
+    privateGame: false,
+  });
+
   return (
     <Grid container direction="column" spacing={GRID_SPACING}>
       <Grid item>
         <Typography variant="h6">{t("Regular game")}</Typography>
       </Grid>
       <Grid item>
-        <Grid container spacing={1} justify="space-between" direction="row">
+        <Grid
+          container
+          spacing={1}
+          justify="space-between"
+          alignItems="center"
+          direction="row"
+        >
           <Grid item>{t("Players")}</Grid>
-          <Grid item>1</Grid>
+          <Grid item className="playersNumber">
+            <ButtonGroup disableElevation>
+              <Button
+                size="small"
+                disabled={false}
+                variant="contained"
+                color={state.playersNumber === 2 ? "primary" : "default"}
+                onClick={() => setState({ ...state, playersNumber: 2 })}
+              >
+                2
+              </Button>
+              <Button
+                disabled={false}
+                variant="contained"
+                color={state.playersNumber === 3 ? "primary" : "default"}
+                onClick={() => setState({ ...state, playersNumber: 3 })}
+              >
+                3
+              </Button>
+              <Button
+                disabled={false}
+                variant="contained"
+                color={state.playersNumber === 4 ? "primary" : "default"}
+                onClick={() => setState({ ...state, playersNumber: 4 })}
+              >
+                4
+              </Button>
+              <Button
+                disabled={false}
+                variant="contained"
+                color={state.playersNumber === 5 ? "primary" : "default"}
+                onClick={() => setState({ ...state, playersNumber: 5 })}
+              >
+                5
+              </Button>
+              <Button
+                disabled={false}
+                variant="contained"
+                color={state.playersNumber === 6 ? "primary" : "default"}
+                onClick={() => setState({ ...state, playersNumber: 6 })}
+              >
+                2x2
+              </Button>
+            </ButtonGroup>
+          </Grid>
         </Grid>
       </Grid>
       <Grid item>
@@ -25,11 +93,37 @@ export default function RegularGameParams() {
         </Grid>
         <Grid container justify="space-between" spacing={GRID_SPACING}>
           <Grid item>{t("Private game")}</Grid>
-          <Grid item>1</Grid>
+          <Grid item>
+            <Switch
+              color="primary"
+              checked={state.privateGame}
+              onChange={() =>
+                setState({
+                  ...state,
+                  privateGame: !state.privateGame,
+                })
+              }
+              name="privateGame"
+              inputProps={{ "aria-label": "secondary checkbox" }}
+            />
+          </Grid>
         </Grid>
         <Grid container justify="space-between" spacing={GRID_SPACING}>
           <Grid item>{t("Game autostart")}</Grid>
-          <Grid item>2</Grid>
+          <Grid item>
+            <Switch
+              color="primary"
+              checked={state.autostart}
+              onChange={() =>
+                setState({
+                  ...state,
+                  autostart: !state.autostart,
+                })
+              }
+              name="autostart"
+              inputProps={{ "aria-label": "secondary checkbox" }}
+            />
+          </Grid>
         </Grid>
       </Grid>
     </Grid>
