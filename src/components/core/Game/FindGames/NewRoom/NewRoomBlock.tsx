8efbe54aa2424efaeee515908d7afac577a087bd
Konstantins <Sysuev>
2020-09-03 15:58:05 +0300

    wfwefwef

--- src/components/core/Game/FindGames/NewRoom/NewRoomBlock.tsx
+++ src/components/core/Game/FindGames/NewRoom/NewRoomBlock.tsx
@@ -3,8 +3,13 @@ import { IRoomState } from "stores/Game/Room/NewRoomStore";
 import React from "react";
 import RoomAvatar from "./RoomAvatar";
 import RoomTypeView from "./RoomTypeView";
+import { concat } from "lodash";
 
 export default function NewRoomBlock({ room }: { room: IRoomState }) {
+  const g = concat(
+    room.players,
+    new Array(room.playersNumber - room.players.length).fill(null)
+  );
   return (
     <Grid
       container
@@ -17,12 +22,14 @@ export default function NewRoomBlock({ room }: { room: IRoomState }) {
         <RoomTypeView room={room} />
       </Grid>
       <Grid item className="newRoomOnePlayers">
-        <Grid container justify="center" direction="column" spacing={1}>
-          {room.players.map((v, k: number) => (
+        <Grid container justify="flex-start" direction="row" spacing={2}>
+          {g.map((v, k: number) => (
             <Grid key={k} item className="newRoomOneAvatar">
               <RoomAvatar
                 avatar={(v && v.avatar) || ""}
                 name={(v && v.name) || ""}
+                onClick={(n) => console.log(22222, n)}
+                roomId={room.roomId}
               />
             </Grid>
           ))}
--- src/components/core/Game/FindGames/NewRoom/RoomAvatar.tsx
+++ src/components/core/Game/FindGames/NewRoom/RoomAvatar.tsx
@@ -1,16 +1,34 @@
+import AddIcon from "@material-ui/icons/Add";
+import { Grid } from "@material-ui/core";
 import { Params } from "config/params";
 import React from "react";
 
 export default function RoomAvatar({
   avatar,
   name,
+  onClick,
+  roomId,
 }: {
   avatar: string;
   name: string;
+  onClick: (id: string) => void;
+  roomId: string;
 }) {
   return (
     <>
-      <img src={`${Params.BASE_URL}/${avatar}`} alt={name} />
+      <Grid
+        container
+        className="avatar"
+        justify="center"
+        alignItems="center"
+        onClick={() => onClick(roomId)}
+      >
+        {avatar ? (
+          <img src={`${Params.BASE_URL}/${avatar}`} alt={name} />
+        ) : (
+          <AddIcon />
+        )}
+      </Grid>
     </>
   );
 }
