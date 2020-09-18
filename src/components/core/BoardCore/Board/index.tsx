a6b74997e2af3da5241cf4a2b798752df8d4b64a
Konstantins <Sysuev>
2020-09-18 13:12:57 +0300

    wefwef

--- src/components/core/BoardCore/Board/index.tsx
+++ src/components/core/BoardCore/Board/index.tsx
@@ -1,6 +1,6 @@
-import { IRoomState, myPlayingRoom$ } from "stores/Game/Rooms/RoomsModel";
 import {
   boardCompleted$,
+  boardGame$,
   boardGate,
 } from "../../../../stores/Game/Board/BoardModel";
 import { useGate, useStore } from "effector-react";
@@ -8,6 +8,7 @@ import { useGate, useStore } from "effector-react";
 import { BoardWrapper } from "./BoardWrapper";
 import GameCompleted from "components/core/Game/GameCompleted/GameCompleted";
 import GameNotFound from "components/core/Game/GameNotFound/GameNotFound";
+import { IRoomState } from "stores/Game/Rooms/RoomsModel";
 import { ModalDialog } from "../../../views/BoardViews/ModalDialog/ModalDialog";
 import React from "react";
 import { RouteComponentProps } from "react-router";
@@ -19,27 +20,23 @@ export const Board = (props: RouteComponentProps | any) => {
   // TODO add loading stores on component mount (not only board)
   useGate(boardGate, { gameId: gameId });
 
-  const playing: IRoomState | null = useStore(myPlayingRoom$);
+  const board: IRoomState | null = useStore(boardGame$);
   const completed: IRoomState | null = useStore(boardCompleted$);
 
   const playerIds =
-    playing && Array.isArray(playing.players)
-      ? playing.players.map((v) => v?.userId || 0)
+    board && Array.isArray(board.players)
+      ? board.players.map((v) => v?.userId || 0)
       : [];
 
-  // console.log(1111, pending);
-  // console.log(2222, playing);
-  // console.log(3333, completed);
-  // console.log(4444, rooms);
   return (
     <>
       <ModalDialog />
       {completed ? (
         <GameCompleted winner={head(completed.players)} />
-      ) : playing ? (
+      ) : board ? (
         <BoardWrapper playerIds={playerIds} />
       ) : (
-        <GameNotFound p={playing} />
+        <GameNotFound p={board} />
       )}
     </>
   );
