4a1df296528dbdcc3e9e7ceefb11804bee2971fb
Konstantins <Sysuev>
2020-09-24 17:43:42 +0300

    wefwef

--- src/components/core/Game/Inventory/Inventory.tsx
+++ src/components/core/Game/Inventory/Inventory.tsx
@@ -1,7 +1,15 @@
 import "./inventory.scss";
 
-import { Button, Grid, Typography } from "@material-ui/core";
+import {
+  Button,
+  FormControl,
+  Grid,
+  InputLabel,
+  Select,
+  Typography,
+} from "@material-ui/core";
 
+import { GRID_SPACING } from "theme";
 import { Link } from "react-router-dom";
 import { Params } from "config/params";
 import React from "react";
@@ -15,46 +23,93 @@ export default function Inventory() {
   const { t } = useTranslation();
   return (
     <>
-      <Template columns={1} title={"Inventory"}>
+      <Template columns={1} title={`${t("Inventory")} ${user?.name}`}>
         <Grid
           container
           alignItems="center"
           justify="space-between"
           direction="row"
-          className={"inventory"}
+          className={"inventoryFilters"}
+          spacing={GRID_SPACING}
         >
           <Grid item>
-            <Grid container spacing={1} alignItems="center">
+            <Grid
+              container
+              alignItems="center"
+              justify="space-between"
+              direction="row"
+              className={"inventoryHeader"}
+            >
               <Grid item>
-                <div className="avatar">
-                  {user?.avatar && (
-                    <Link to={`/profile/${user.userId}`}>
-                      <img
-                        alt={user.name}
-                        src={Params.BASE_URL + "/" + user.avatar}
-                      />
+                <Grid container spacing={1} alignItems="center">
+                  <Grid item>
+                    <div className="avatar">
+                      {user?.avatar && (
+                        <Link to={`/profile/${user.userId}`}>
+                          <img
+                            alt={user.name}
+                            src={Params.BASE_URL + "/" + user.avatar}
+                          />
+                        </Link>
+                      )}
+                    </div>
+                  </Grid>
+                  <Grid item>
+                    <Link to={`/profile/${user?.userId}`}>
+                      <Typography className="profileName">
+                        {(user && user.name.toUpperCase()) || ""}
+                      </Typography>
                     </Link>
-                  )}
-                </div>
+                  </Grid>
+                  <Grid item>
+                    <Typography color="textSecondary" variant="h6">
+                      {">"}
+                    </Typography>
+                  </Grid>
+                  <Grid item>{t("Inventory")}</Grid>
+                  <Grid item>
+                    <Typography color="textSecondary">12</Typography>
+                  </Grid>
+                </Grid>
               </Grid>
               <Grid item>
-                <Link to={`/profile/${user?.userId}`}>
-                  <Typography className="profileName">
-                    {(user && user.name.toUpperCase()) || ""}
-                  </Typography>
-                </Link>
-              </Grid>
-              <Grid item>{">"}</Grid>
-              <Grid item>{t("Inventory")}</Grid>
-              <Grid item>
-                <Typography color="textSecondary">12</Typography>
+                <Button variant="outlined" color="primary">
+                  {t("Exchange offers")}
+                </Button>
               </Grid>
             </Grid>
           </Grid>
           <Grid item>
-            <Button variant="outlined" color="primary">
-              {t("Exchange offers")}
-            </Button>
+            <Grid
+              container
+              alignItems="center"
+              justify="space-between"
+              direction="row"
+              className={"inventoryFilters"}
+              spacing={GRID_SPACING}
+            >
+              <Grid item>
+                <FormControl variant="outlined" className={"from"}>
+                  <InputLabel htmlFor="filled-age-native-simple">
+                    Age
+                  </InputLabel>
+                  <Select
+                    native
+                    value={1}
+                    onChange={() => console.log(4)}
+                    inputProps={{
+                      name: "age",
+                      id: "filled-age-native-simple",
+                    }}
+                  >
+                    <option aria-label="None" value="" />
+                    <option value={10}>Ten</option>
+                    <option value={20}>Twenty</option>
+                    <option value={30}>Thirty</option>
+                  </Select>
+                </FormControl>
+              </Grid>
+            </Grid>{" "}
           </Grid>
         </Grid>
       </Template>
