6b2fa7c2065095b0eca377f46e29fc216a07376c
Konstantins <Sysuev>
2020-09-30 17:34:57 +0300

    ertgeg

--- src/components/core/BoardCore/Board/BoardWrapper.tsx
+++ src/components/core/BoardCore/Board/BoardWrapper.tsx
@@ -155,48 +155,53 @@ export const BoardWrapper = ({ board }: { board: IRoomState }) => {
 
   return (
     <>
-      {players && fields && tokens && user && (
-        <div
-          className="boardWrapper"
-          style={{ width: "100%", height: "100%" }}
-          mnpl-newcolors="1"
-          cz-shortcut-listen="true"
-          mnpl-mouse="1"
-          data-os="null"
-          data-browser="chrome"
-        >
-          <div className="table _shakehack">
-            <div className="table-body">
-              <Players players={players} />
-              <div className="table-body-board">
-                <BoardView
-                  fields={Array.isArray(fields) ? fields : []}
-                  fieldActionId={fieldActionId}
-                  contract={contract}
-                  user={user}
-                />
+      {players &&
+        players.length &&
+        fields.length &&
+        tokens.length &&
+        user &&
+        user.userId && (
+          <div
+            className="boardWrapper"
+            style={{ width: "100%", height: "100%" }}
+            mnpl-newcolors="1"
+            cz-shortcut-listen="true"
+            mnpl-mouse="1"
+            data-os="null"
+            data-browser="chrome"
+          >
+            <div className="table _shakehack">
+              <div className="table-body">
+                <Players players={players} />
+                <div className="table-body-board">
+                  <BoardView
+                    fields={Array.isArray(fields) ? fields : []}
+                    fieldActionId={fieldActionId}
+                    contract={contract}
+                    user={user}
+                  />
 
-                <div className="table-body-board-center">
-                  <M1tv />
-                  {modal()}
-                  <Arbitr />
-                  <Ticket />
-                  <Chat />
+                  <div className="table-body-board-center">
+                    <M1tv />
+                    {modal()}
+                    <Arbitr />
+                    <Ticket />
+                    <Chat />
+                  </div>
+                  <div className="table-body-board-tokens">
+                    <Tokens tokens={tokens} fields={fields} />
+                  </div>
+                  <Dices />
+                  <Contract user={user} contract={contract} />
+                  <TableHelper />
                 </div>
-                <div className="table-body-board-tokens">
-                  <Tokens tokens={tokens} fields={fields} />
-                </div>
-                <Dices />
-                <Contract user={user} contract={contract} />
-                <TableHelper />
               </div>
-            </div>
 
-            <div id="placeholder_gameover"></div>
+              <div id="placeholder_gameover"></div>
+            </div>
+            <div className="table-jokes"></div>
           </div>
-          <div className="table-jokes"></div>
-        </div>
-      )}
+        )}
 
       <ToastContainer />
     </>
