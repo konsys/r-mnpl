fff08b5cbde592caf773830b5c3d10bc3b03d4c0
Konstantins <Sysuev>
2020-10-16 18:29:22 +0300

    jhvv

--- src/components/views/BoardViews/Avatar/__tests__/avatar.test.tsx
+++ src/components/views/BoardViews/Avatar/__tests__/avatar.test.tsx
@@ -1,11 +1,19 @@
 import { Avatar } from "../Avatar";
-import { Params } from "../../../../../config/params";
 import React from "react";
 import renderer from "react-test-renderer";
 import { shallow } from "enzyme";
 
 describe("Test Avatar view", () => {
   const avatar = (
+    <Avatar
+      money={999889}
+      remainTime={38}
+      isVip={true}
+      avatar={"testAvatareSrc"}
+      name={"Ivan"}
+    />
+  );
+  const notVipAvatar = (
     <Avatar
       money={1}
       remainTime={2}
@@ -14,6 +22,7 @@ describe("Test Avatar view", () => {
       name={"Ivan"}
     />
   );
+
   it("renders correctly", () => {
     const tree = renderer.create(avatar).toJSON();
     expect(tree).toMatchSnapshot();
@@ -23,10 +32,28 @@ describe("Test Avatar view", () => {
     expect(shallow(avatar).contains("Ivan")).toBeTruthy();
   });
 
-  it("shows img src", () => {
-    const wrapper = shallow(avatar);
-    expect(
-      wrapper.find(".table-body-players-card-body-avatar-background")
-    ).toHaveLength(1);
+  it("shows money", () => {
+    expect(shallow(avatar).contains(999889)).toBeTruthy();
+  });
+
+  it("shows remainTime", () => {
+    expect(shallow(avatar).contains(38)).toBeTruthy();
+  });
+
+  it("tests vip div", () => {
+    const tree = renderer
+      .create(
+        <Avatar
+          money={1}
+          remainTime={2}
+          isVip={false}
+          avatar={"testAvatareSrc"}
+          name={"Ivan"}
+        />
+      )
+      .toJSON();
+
+    expect(shallow(avatar).find("._vip")).toHaveLength(1);
+    expect(shallow(notVipAvatar).find("._vip")).toHaveLength(0);
   });
 });
