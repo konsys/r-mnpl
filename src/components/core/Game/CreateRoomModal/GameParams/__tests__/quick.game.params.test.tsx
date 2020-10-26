a115f0c292bd476a8cffb6ef98de29762c69235a
Konstantins <Sysuev>
2020-10-26 10:47:17 +0300

    wedfwef

--- src/components/core/Game/CreateRoomModal/GameParams/__tests__/quick.game.params.test.tsx
+++ src/components/core/Game/CreateRoomModal/GameParams/__tests__/quick.game.params.test.tsx
@@ -27,20 +27,33 @@ describe("Test quick geame params", () => {
     expect(shall(<QuickGameParams isVip />).find(RoomSwitch)).toHaveLength(3);
   });
 
+  it("should render RoomSwitch vip", () => {
+    shall(<QuickGameParams isVip />)
+      .find(RoomSwitch)
+      .forEach((v) => expect(v.prop("disabled")).toBeFalsy());
+  });
+
+  it("should render RoomSwitch not vip", () => {
+    shall(<QuickGameParams isVip={false} />)
+      .find(RoomSwitch)
+      .forEach((v) => expect(v.prop("disabled")).toBeTruthy());
+  });
+
   it("should render 4 FormControlLabels", () => {
     expect(
       shall(<QuickGameParams isVip />).find(FormControlLabel)
     ).toHaveLength(4);
   });
 
-  it("should render not vip", () => {
-    shall(<QuickGameParams isVip={false} />)
-      .find(FormControlLabel)
-      .forEach((v) => expect(v.prop("disabled")).toBeTruthy());
-  });
-  it("should render  vip", () => {
+  it("should render FormControlLabel vip", () => {
     shall(<QuickGameParams isVip />)
       .find(FormControlLabel)
       .forEach((v) => expect(v.prop("disabled")).toBeFalsy());
+  });
+
+  it("should render not FormControlLabel vip", () => {
+    shall(<QuickGameParams isVip={false} />)
+      .find(FormControlLabel)
+      .forEach((v) => expect(v.prop("disabled")).toBeTruthy());
   });
 });
