37cab3b08a8d137f370aef72f91ef21ecd5de3a7
Konstantins <Sysuev>
2020-10-26 08:17:49 +0300

    wefwef

--- src/components/core/Game/CreateRoomModal/GameParams/views/__tests__/players.number.test.tsx
+++ src/components/core/Game/CreateRoomModal/GameParams/views/__tests__/players.number.test.tsx
@@ -1,12 +1,30 @@
+import { Button } from "@material-ui/core";
 import PlayersNumber from "../PlayersNumber";
 import React from "react";
-import { shallow } from "enzyme";
+import { createShallow } from "@material-ui/core/test-utils";
 
 describe("Players number test", () => {
+  let shallow: any;
+
+  beforeAll(() => {
+    shallow = createShallow();
+  });
+
+  afterAll(() => {
+    shallow.cleanUp();
+  });
+
   it("should render vip", () => {
     expect(shallow(<PlayersNumber isVip />)).toMatchSnapshot();
   });
   it("should render not vip", () => {
     expect(shallow(<PlayersNumber isVip={false} />)).toMatchSnapshot();
+  });
+
+  it("should render 5 buttons", () => {
+    console.log(shallow(<PlayersNumber isVip={false} />).debug());
+    expect(shallow(<PlayersNumber isVip={false} />).find(Button)).toHaveLength(
+      5
+    );
   });
 });
