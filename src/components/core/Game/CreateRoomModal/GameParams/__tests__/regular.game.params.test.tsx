f361f7025df58d62a12060bb3b667bc3dfd52e33
Konstantins <Sysuev>
2020-10-26 10:49:34 +0300

    dfdg

--- src/components/core/Game/CreateRoomModal/GameParams/__tests__/regular.game.params.test.tsx
+++ src/components/core/Game/CreateRoomModal/GameParams/__tests__/regular.game.params.test.tsx
@@ -1,9 +1,38 @@
 import React from "react";
 import RegularGameParams from "../RegularGameParams";
+import RoomSwitch from "../views/RoomSwitch";
+import { createShallow } from "@material-ui/core/test-utils";
 import { shallow } from "enzyme";
 
 describe("Test quick geame params", () => {
+  let shall: typeof shallow;
+
+  beforeAll(() => {
+    shall = createShallow();
+  });
+
+  afterAll(() => {
+    // @ts-ignore
+    shall.cleanUp();
+  });
+
   it("should render", () => {
     expect(shallow(<RegularGameParams isVip />)).toMatchSnapshot();
+  });
+
+  it("should render 2 switches", () => {
+    expect(shall(<RegularGameParams isVip />).find(RoomSwitch)).toHaveLength(2);
+  });
+
+  it("should render RoomSwitch vip", () => {
+    shall(<RegularGameParams isVip />)
+      .find(RoomSwitch)
+      .forEach((v) => expect(v.prop("disabled")).toBeFalsy());
+  });
+
+  it("should render RoomSwitch vip", () => {
+    shall(<RegularGameParams isVip={false} />)
+      .find(RoomSwitch)
+      .forEach((v) => expect(v.prop("disabled")).toBeTruthy());
   });
 });
