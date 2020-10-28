c1bd96b8542beed206a4c3d07b7b5875c653c2d0
Konstantins <Sysuev>
2020-10-28 18:59:05 +0300

    ewrferwf

--- src/components/core/Game/Inventory/__tests__/inventory.item.tet.tsx
+++ src/components/core/Game/Inventory/__tests__/inventory.item.tet.tsx
@@ -8,4 +8,33 @@ describe("test inventory item", () => {
       shallow(<InventoryItem img={"testImage"} level={1} name={"testName"} />)
     ).toMatchSnapshot();
   });
+
+  it("should render backgroundImg", () => {
+    expect(
+      shallow(<InventoryItem img={"testImage"} level={1} name={"testName"} />)
+        .find("div")
+        .get(2).props.style
+    ).toHaveProperty("backgroundImage", `url(testImage)`);
+  });
+
+  it("should render backgroundImg", () => {
+    expect(
+      shallow(<InventoryItem img={"testImage"} level={1} name={"testName"} />)
+        .find("div")
+        .get(4).props.children
+    ).toBe("testName");
+  });
+
+  it("should render backgroundImg", () => {
+    expect(
+      shallow(<InventoryItem img={"testImage"} level={1} name={"testName"} />)
+        .find("div")
+        .get(3).props.className
+    ).toStrictEqual(`itemAbout level_${1}`);
+    expect(
+      shallow(<InventoryItem img={"testImage"} level={2} name={"testName"} />)
+        .find("div")
+        .get(3).props.className
+    ).toStrictEqual(`itemAbout level_${2}`);
+  });
 });
