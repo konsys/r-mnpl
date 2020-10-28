83aad0ffc8d55985ca56ea4852bc35dcc1cc508a
Konstantins <Sysuev>
2020-10-28 10:54:42 +0300

    wefwef

---
+++ src/components/core/Game/GameChat/__tests__/chat.input.adorment.test.tsx
@@ -0,0 +1,45 @@
+import * as chat from "stores/Game/Chat/GameChatModel";
+
+import { mount, shallow } from "enzyme";
+
+import ChatInputAdornment from "../ChatInputAdornment";
+import { Delete } from "@material-ui/icons";
+import PlayerChip from "../PlayerChip";
+import React from "react";
+import { addReplyToEvent } from "stores/Game/Chat/GameChatModel";
+import { testUser } from "testMocks/user";
+
+describe("test input adorment", () => {
+  it("should render", () => {
+    expect(shallow(<ChatInputAdornment />)).toMatchSnapshot();
+  });
+  it("shouldn't render replies", () => {
+    expect(shallow(<ChatInputAdornment />).find(PlayerChip)).toHaveLength(0);
+  });
+
+  it("shouldn't render replies", () => {
+    addReplyToEvent(testUser);
+    expect(shallow(<ChatInputAdornment />).find(PlayerChip)).toHaveLength(1);
+  });
+
+  it("should render replies", () => {
+    addReplyToEvent(testUser);
+    expect(
+      shallow(<ChatInputAdornment />)
+        .find(PlayerChip)
+        .get(0).props.name
+    ).toBe(testUser.name);
+  });
+
+  it("should delete chip", () => {
+    addReplyToEvent(testUser);
+    const testMock = chat as any;
+    const deleteMock = jest.spyOn(testMock, "deleteReplyToEvent");
+    shallow(<ChatInputAdornment />)
+      .find(PlayerChip)
+      .get(0)
+      .props.handleDelete();
+
+    expect(deleteMock).toHaveBeenCalledTimes(1);
+  });
+});
