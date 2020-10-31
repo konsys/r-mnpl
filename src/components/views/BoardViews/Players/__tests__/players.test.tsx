bea04ab72ca508001a76920e4882fef10b91c1d7
Konstantins <Sysuev>
2020-10-31 19:01:21 +0300

    всвсвс

--- src/components/views/BoardViews/Players/__tests__/players.test.tsx
+++ src/components/views/BoardViews/Players/__tests__/players.test.tsx
@@ -1,5 +1,6 @@
-import { testPlayer1, testUser } from "testMocks/user";
+import { testPlayer1, testPlayer2, testUser } from "testMocks/user";
 
+import { Avatar } from "../../Avatar/Avatar";
 import { Players } from "../Players";
 import React from "react";
 import { shallow } from "enzyme";
@@ -10,11 +11,23 @@ describe("Room top five test", () => {
     expect(
       shallow(
         <Players
-          players={[testPlayer1]}
+          players={[testPlayer1, testPlayer2]}
           user={testUser}
           action={testDoNothingAction}
         />
       )
     ).toMatchSnapshot();
+  });
+
+  it("should render all players", () => {
+    expect(
+      shallow(
+        <Players
+          players={[testPlayer1, testPlayer2]}
+          user={testUser}
+          action={testDoNothingAction}
+        />
+      ).find(Avatar)
+    ).toHaveLength(2);
   });
 });
