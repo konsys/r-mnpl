6827278c8e59ebb49c6bda32652ae258396e15cc
Konstantins <Sysuev>
2020-11-01 22:20:09 +0300

    wefwef

--- src/components/views/BoardViews/Tokens/__tests__/tokens.test.tsx
+++ src/components/views/BoardViews/Tokens/__tests__/tokens.test.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import { Tokens } from "../Tokens";
 import { shallow } from "enzyme";
+import { testField } from "testMocks/field";
+import { testToken } from "testMocks/tokens";
 
-describe("Table helper  test", () => {
+describe("Tokens test", () => {
   it("should render", () => {
-    expect(shallow(<Tokens />)).toMatchSnapshot();
+    expect(
+      shallow(<Tokens fields={[testField]} tokens={[testToken]} />)
+    ).toMatchSnapshot();
   });
 });
---
+++ src/testMocks/tokens.ts
@@ -0,0 +1,9 @@
+import { IToken } from "types/types";
+
+export const testToken: IToken = {
+  userId: 1,
+  left: 10,
+  top: 10,
+  jailed: 0,
+  meanPosition: 101,
+};
