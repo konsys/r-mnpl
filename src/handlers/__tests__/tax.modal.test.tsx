9fb028098542d6a581d5f1470fa00b8aeb996c68
Sysuev <KSysuev@luxoft.com>
2020-11-14 21:15:37 +0300

    trtrg

--- src/handlers/__tests__/tax.modal.test.tsx
+++ src/handlers/__tests__/tax.modal.test.tsx
@@ -1,6 +1,8 @@
 import { taxModal } from "handlers/Modals";
 import * as actions from "stores/Board/ActionStore";
+import { setPlayersEvent } from "stores/Board/PlayersStore";
 import { testRollDicesModal } from "testMocks/action";
+import { testPlayer1 } from "testMocks/user";
 import { OutcomeMessageType } from "types/types";
 describe("Modals test", () => {
   beforeEach(() => jest.clearAllMocks());
@@ -36,9 +38,43 @@ describe("Modals test", () => {
   });
 
   it("should send pay action", () => {
+    const res = taxModal({ ...testRollDicesModal, money: 123456 });
     // @ts-ignore
-    const res = taxModal({ ...testRollDicesModal, money: null });
+    expect(res.actionButtons[0].title).toBe("Заплатить 123456k");
+  });
+
+  it("should send pay action", () => {
+    setPlayersEvent({ version: 1, players: [{ ...testPlayer1, money: 999 }] });
     // @ts-ignore
-    expect(res.actionButtons[0].title).toBe("Заплатить 0k");
+    const res = taxModal({ ...testRollDicesModal, money: 1000 });
+    // @ts-ignore
+    expect(res.actionButtons[0].disabled).toBe(true);
+  });
+
+  it("should send pay action", () => {
+    setPlayersEvent({ version: 1, players: [{ ...testPlayer1, money: 999 }] });
+    // @ts-ignore
+    const res = taxModal({ ...testRollDicesModal, money: -1000 });
+    // @ts-ignore
+    expect(res.actionButtons[0].disabled).toBe(true);
+  });
+
+  it("should send pay action", () => {
+    setPlayersEvent({ version: 1, players: [{ ...testPlayer1, money: 1000 }] });
+    // @ts-ignore
+    const res = taxModal({ ...testRollDicesModal, money: 999 });
+    // @ts-ignore
+    expect(res.actionButtons[0].disabled).toBe(false);
+  });
+
+  it("should send pay action", () => {
+    setPlayersEvent({
+      version: 1,
+      players: [{ ...testPlayer1, money: 1000 }],
+    });
+    // @ts-ignore
+    const res = taxModal({ ...testRollDicesModal, money: -999 });
+    // @ts-ignore
+    expect(res.actionButtons[0].disabled).toBe(false);
   });
 });
