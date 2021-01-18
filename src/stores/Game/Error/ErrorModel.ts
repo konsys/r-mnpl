09113d941c83a0a378d664ef39de1377667533fd
konstantin sysuev <harry-run@yandex.ru>
2021-01-18 16:47:42 +0300

    wefwef

--- src/stores/Game/Error/ErrorModel.ts
+++ src/stores/Game/Error/ErrorModel.ts
@@ -7,7 +7,14 @@ export const setError = createEvent<number>();
 
 export const error$ = createStore<number>(0)
   .on(setError, (_, v: any) => {
-    if (v.error.response && v.error.response.data) {
+    if (v.error.response && v.error.response.data && v.error.response.message) {
+      console.log(22222222222222, v.error.response.data.message);
+      return v.error.response.data.message;
+    } else if (
+      v.error.response &&
+      v.error.response.data &&
+      v.error.response.data.code
+    ) {
       console.log(333333333333, v.error.response.data.code);
       return v.error.response.data.code;
     } else {
@@ -17,8 +24,9 @@ export const error$ = createStore<number>(0)
   .reset(clearError);
 
 error$.updates.watch((code: number) => {
-  console.log(2222222222222, code);
+  console.log(2222222222222, ErrorCode[code]);
   notify(code);
 });
 
-const notify = (code: number) => toast(ErrorCode[code]);
+const notify = (code: number) =>
+  toast(ErrorCode[code], { onClose: () => clearError() });
